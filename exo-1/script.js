0c8340e23e632b596f2ee37dd9100ea620cd6346
Lyoubomyr-Didyk <lyoubomyr.didyk@le-campus-numerique.fr>
2023-05-23 15:29:04 +0200

    fin exo 1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,8 @@
 // you can write js here
 console.log('hello from file');
+
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+const celsius = kelvins - 273;
+console.log("Aujourd'hui la température est de:",celsius,"celsius");
+const fahrenheits = celsius * (9/5) +32;
+console.log("Aujourd'hui la température est de:",Math.floor(fahrenheits),"fahrenheits");
