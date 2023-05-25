e3353351868277d426252d536cfe8fa0b6443664
Lyoubomyr-Didyk <lyoubomyr.didyk@le-campus-numerique.fr>
2023-05-25 15:18:27 +0200

    fin exo 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -2,7 +2,7 @@
 
 console.log('exo-5');
 
-let input = "Lyoubomyr";
+let input = prompt("Write something")
 
 const vowels = ["a", "e", "i", "o", "u", "y"];
 
@@ -13,10 +13,13 @@ for(i=0; i < input.length; i ++){
 console.log(i + " = " + input[i])
     for(j = 0; j < vowels.length; j ++){
         console.log(vowels[j]);
-
+    if (input[i] == vowels[j]){
+        resultArray.push(input[i]);
     }
+    } 
   }
-   
+console.log(resultArray.join('').toUpperCase());
+
 
 
 /*for(i=0; i < input.length; i += 1){
