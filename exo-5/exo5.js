f533d4b13d10598cfb3890124a349ba17df72288
Lyoubomyr-Didyk <lyoubomyr.didyk@le-campus-numerique.fr>
2023-05-25 14:56:26 +0200

    add 2 loops for

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,26 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = "Lyoubomyr";
+
+const vowels = ["a", "e", "i", "o", "u", "y"];
+
+let resultArray = [];
+
+
+for(i=0; i < input.length; i ++){
+console.log(i + " = " + input[i])
+    for(j = 0; j < vowels.length; j ++){
+        console.log(vowels[j]);
+
+    }
+  }
+   
+
+
+/*for(i=0; i < input.length; i += 1){
+    console.log(i + " = " + input[i])
+    console.log(vowels.indexOf(input[i]));
+       
+    }*/
